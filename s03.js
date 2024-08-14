6669d2c5ed611f92350ef55a0926254c47d9b4b5
Okan Patır <relreburada@gmail.com>
2024-08-14 19:11:01 +0300

    Create s03.js

---
+++ s03.js
@@ -0,0 +1,6 @@
+function joinArraysById(arr1, arr2) {
+  const mergedObj = {}
+  arr1.forEach(a => mergedObj[a.id] = a)
+  arr2.forEach(a => mergedObj[a.id] = {...mergedObj[a.id], ...a})
+  return Object.values(mergedObj)
+}
