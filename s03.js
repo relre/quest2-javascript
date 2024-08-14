8b9db0212199e563e982ec1adc05d95bfdc527d2
Okan Patır <relreburada@gmail.com>
2024-08-14 19:26:21 +0300

    Update s03.js

--- s03.js
+++ s03.js
@@ -1,6 +1,8 @@
 function joinArraysById(arr1, arr2) {
-  const mergedObj = {}
-  arr1.forEach(a => mergedObj[a.id] = a)
+  // Your solution
+  const mergedObj = {};
+  arr1.forEach(a => mergedObj[a.id] = a);
   arr2.forEach(a => mergedObj[a.id] = {...mergedObj[a.id], ...a})
-  return Object.values(mergedObj)
+  
+  return Object.values(mergedObj);
 }
