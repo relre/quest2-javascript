da470f06db633d0b8e6ab3048aa0ad20f724c79b
Okan Patır <relreburada@gmail.com>
2024-08-14 18:40:18 +0300

    Update s02.js

--- s02.js
+++ s02.js
@@ -1,14 +1,19 @@
+// return an array of strings, every string being part of text no longer than max
 function splitText(text, max) {
-  if(!text.length) return [];
+  // good luck
+  if(!text) return [];
   let result = [];
   let last = 0;
-  for(let i = 0; i < text.length; i++) {
-    if(text[i] == " ") { last = i; }
+
+  for (let i=0; i<text.length;i++) {
+    if(text[i] == ' ') last = i;
     if(i == max) {
       result.push(text.slice(0, last));
-      text = text.slice(last+1);
+      text = text.slice(last+1)
       i = 0;
     }
   }
+  
   result.push(text);
-  return result;
+  return result
+}
